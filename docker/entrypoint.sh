66b5122294707c2220ba6145cc792866c4b28fb1
Kim-JeongHan <kimjh9813@naver.com>
2024-03-24 20:25:00 +0900

    Update vscode extension

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -21,5 +21,21 @@ then
   echo "Sourced overlay workspace"
 fi
 
+export ROS_DOMAIN_ID=7
+#export ROS_NAMESPACE=robot1
+
+#export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+# export RMW_IMPLEMENTATION=rmw_connext_cpp
+# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+# export RMW_IMPLEMENTATION=rmw_gurumdds_cpp
+
+export RCUTILS_CONSOLE_OUTPUT_FORMAT='[{severity} {time}] [{name}]: {message} ({function_name}() at {file_name}:{line_number})'
+#export RCUTILS_CONSOLE_OUTPUT_FORMAT='[{severity}]: {message}'
+export RCUTILS_COLORIZED_OUTPUT=1
+export RCUTILS_LOGGING_USE_STDOUT=0
+export RCUTILS_LOGGING_BUFFERED_STREAM=1
+
+alias killgazebo='killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient'
+
 # Execute the command passed into this entrypoint
 exec "$@"
